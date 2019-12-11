e237c0e0d91caf15ff9acb9da4f701a19e0bdc96
omar jakmira <poweromar19942@gmail.com>
2019-12-11 21:00:37 +0200

    3d fox

--- app/src/main/java/com/example/arcoreapplication/MainActivity.kt
+++ app/src/main/java/com/example/arcoreapplication/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.arcoreapplication
 
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 
+@RequiresApi(Build.VERSION_CODES.N)
 class MainActivity : AppCompatActivity() {
 
     lateinit var arFragment: ArFragment
@@ -13,6 +21,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         arFragment = supportFragmentManager.findFragmentById(R.id.ar_fragment) as ArFragment
+
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            placeModel(hitResult.createAnchor())
+        }
+    }
+
+    private fun placeModel(anchor: Anchor?) {
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("ArcticFox_Posed.sfb"))
+            .setRegistryId("3DMODEL")
+            .build()
+            .thenAccept {
+                addNodeToScene(it,anchor)
+            }.exceptionally {
+                var builder = AlertDialog.Builder(this@MainActivity)
+                builder.setMessage(it.message)
+                builder.show()
+                null
+            }
+
+    }
+
+    private fun addNodeToScene(modelRenderable: ModelRenderable?, anchor: Anchor?) {
+        var node = AnchorNode(anchor)
+        node.renderable = modelRenderable
+        arFragment.arSceneView.scene.addChild(node)
     }
 
 }
